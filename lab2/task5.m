612a4b5376a16aa0c243faf9b114ed3c1dfc39c3
klashenriksson <6956114+klashenriksson@users.noreply.github.com>
2021-09-26 13:34:53 +0200

    ajhd

--- lab2/task5.m
+++ lab2/task5.m
@@ -24,6 +24,46 @@ figure;
 p = plot(final_net);
 highlight(p, many_infected_nodes);
 
+%% many sims
+rho = 2;
+mu = 2;
+beta = 1;
+iters = 2000;
+t_end = 500;
+initial_infect_fract = 0.3;
+close all
+
+W = [load("nets/pownet_N100_gamma-2.5.mat"); 
+    load("nets/pownet_N1000_gamma-2.5.mat");
+    load("nets/pownet_N10000_gamma-2.5.mat");
+    ];
+
+adj_mats = {};
+for i = 1:numel(W)
+    mat = W(i).W;
+    mat(mat ~= 0) = 1;
+    adj_mats{end+1} = mat;
+end
+
+g10_2 = graph(cell2mat(adj_mats(1)));
+g10_3 = graph(cell2mat(adj_mats(2)));
+g10_4 = graph(cell2mat(adj_mats(3)));
+
+g10_2 = init_graph(g10_2,rho,initial_infect_fract);
+g10_3 = init_graph(g10_3,rho,initial_infect_fract);
+g10_4 = init_graph(g10_4,rho,initial_infect_fract);
+
+g10_2 = stoch_sim_over_network(g10_2, mu,beta, 0, t_end, iters);
+g10_3 = stoch_sim_over_network(g10_3, mu,beta, 0, t_end, iters/10);
+g10_4 = stoch_sim_over_network(g10_4, mu,beta, 0, t_end, iters/100);
+
+plot(sum(g10_2.Nodes.Population), sum(g10_2.Nodes.I)./sum(g10_2.Nodes.Population),'*');
+hold on
+plot(sum(g10_3.Nodes.Population), sum(g10_3.Nodes.I)./sum(g10_3.Nodes.Population),'*');
+hold on
+plot(sum(g10_4.Nodes.Population), sum(g10_4.Nodes.I)./sum(g10_4.Nodes.Population),'*');
+
+
 %%
 
 function g = init_graph(g,rho, infected_fraction)
