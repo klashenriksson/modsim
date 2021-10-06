9c2f214ba8f1a1e45a9a49c2f1715601753d18cb
klashenriksson <6956114+klashenriksson@users.noreply.github.com>
2021-10-06 17:12:38 +0200

    jkasfs

--- ModSim_LabStoch/src/mc.c
+++ ModSim_LabStoch/src/mc.c
@@ -7,7 +7,8 @@
 
 int mc_sweep(Par *par, double *pos)
 {
-    double newpos[D], dist[D];
+    double newpos[D];
+    double dist[D];
     double new_positions[D*par->n];
     int naccept = 0;
 
@@ -15,29 +16,31 @@ int mc_sweep(Par *par, double *pos)
     {
         for (int d = 0; d < D; d++)
         {
-            newpos[d] = inrange(pos[i * D + d] + dran_sign() * par->b, par->L[d]);
+            double p = pos[i * D + d] + dran_sign() * par->b;
+            newpos[d] = inrange(p, par->L[d]);
         }
 
         double delta_pot = 0.f;
         for (int j = 0; j < par->n; j++)
         {
             if (i == j)
+            {
                 continue;
+            }
                 
             double old_r2 = dist2(par->L, pos + D * i, pos + D * j, dist);
-            double old_pot = 0.f;
+            double old_pot = 0.;
             if (old_r2 < CUT * CUT) 
             {
                 old_pot = pair_interaction(old_r2);
             }
 
             double new_r2 = dist2(par->L, newpos, pos + D * j, dist);
-            double new_pot = 0.f;
+            double new_pot = 0.;
             if (new_r2 < CUT * CUT)
             {
                 new_pot = pair_interaction(new_r2);
             }
-
             delta_pot += new_pot - old_pot;
         }
 
@@ -47,7 +50,7 @@ int mc_sweep(Par *par, double *pos)
         {
             for (int d = 0; d < D; d++)
             {
-                new_positions[D * i + d] = inrange(newpos[d], par->L[d]);
+                new_positions[D * i + d] = newpos[d];
             }
             naccept++;
         }
